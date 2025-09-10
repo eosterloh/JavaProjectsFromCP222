4e6847fb37f3053b94f19b46bae7e98a091204fd
Erick Osterloh <eosterloh@gmail.com>
2025-09-09 18:12:08 -0600

    Delete day4_hr2/src directory

--- day4_hr2/src/CantDoThatException.java
+++
@@ -1,5 +0,0 @@
-public class CantDoThatException extends Exception {
-    public CantDoThatException(String msg) {
-        super(msg); // Make sure that the stack trace an other Exception stuff is setup
-    }
-}
